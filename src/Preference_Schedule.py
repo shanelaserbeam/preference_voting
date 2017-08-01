646e54d7086535b79d39499a2ea352743ec19ad5
shanelaserbeam <shanelaserbeam@gmail.com>
2017-08-01 15:09:32 -0500

    initial

--- src/Preference_Schedule.py
+++ src/Preference_Schedule.py
@@ -1,10 +1,29 @@
 import numpy as np
 
 class PreferenceSchedule():
+    __version__ = "0.0.1"
+
     def __init__(self):
+        self.num_cands = None
+        self.candidates = None
+        self.ranks = None
+        self.schedule = None
+        self.number_per_ballot_type = None
+
         pass
 
     def __str__(self):
         pass
 
-    
+    def get_generic_candidates(self, num_cands):
+            default_cands = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            self.candidates = np.array(list(default_cands[:num_cands]))
+            print self.candidates
+
+    def generic_vote(self, num_candidates=5, num_ranks=None, num_ballots=5):
+        if num_ranks == None:  #uses number of candidates as number of ranks by default
+            num_ranks = num_candidates
+        self.get_generic_candidates(num_candidates)
+
+    def get_ballots(self, num_ballots):
+        pass
---
+++ src/testy.py
@@ -0,0 +1,9 @@
+from Preference_Schedule import PreferenceSchedule
+
+ps1 = PreferenceSchedule()
+
+
+
+ps1.get_generic_candidates(9)
+
+print PreferenceSchedule.__version__
